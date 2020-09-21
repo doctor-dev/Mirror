4681f49fe2697e0d6428b778956a9e6fd3b8c7f9
doctor-dev <atharva.rajadhyaksha19@vit.edu>
2020-09-21 18:11:03 +0530

    Key event

--- Mirror/Mirror/Events/Event.h
+++ Mirror/Mirror/Events/Event.h
@@ -26,5 +26,48 @@ namespace Mirror {
 	                           virtual EventType GetEventType() const override { return GetStaticType(); }\
 							   virtual const char* GetName() const override {return ##type;}
 
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
 
+	class MIRROR_API Event {
+		friend class EventDispatcher;
+	public:
+		virtual EventType GetEventType() const = 0;
+		virtual const char* GetName() const = 0;
+		virtual int GetCategoryFlags() const = 0;
+		virtual std::string Tostring() { return Getname(); };
+		inline bool IsInCategory(EventCategory category) {
+			return GetCategoryFlags() & category;
+		}
+	protected:
+		bool m_handled = false;
+	};
+
+	class EventDispatcher {
+		template<typename T>
+		using EventFn = std::function<bool(t&)>;
+
+	public:
+		EventDispatcher(Event& Event)
+			:m_Event(event)
+		{
+
+		}
+
+		template<typename T>
+		bool Dispatch(EventFn<t> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType()) {
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			else return false;
+		}
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, const Event& e)
+	{
+		return os << e.ToString();
+	}
 }
