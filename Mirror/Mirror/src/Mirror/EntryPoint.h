0fe97d215bef8ffb5db6f159ddd17a8e5c5467d2
doctor-dev-lap <lap+doctor-dev@users.noreply.github.com>
2020-12-02 00:30:45 +0530

    Revert "Final Update"
    This reverts commit b9167ab2d04896c254ff1dec069e38704df875ad.

--- Mirror/Mirror/src/Mirror/EntryPoint.h
+++ Mirror/Mirror/src/Mirror/EntryPoint.h
@@ -6,7 +6,9 @@ extern Mirror::Application* Mirror::CreateApplication();
 int main(int argc,char** argv)
 {
     Mirror::Log::Init();
-    
+    MR_CORE_WARN("Initialized Log!");
+    int a = 5;
+    MR_INFO("Hello! Var={0}", a);
     
    
     auto app = Mirror::CreateApplication();
